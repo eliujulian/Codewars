4cf6266abd16ca4618373412a89cc7bd39479552
Eliu Schmitt <mail@eliu.de>
2015-09-27 13:22:40 +0200

    inital commit

---
+++ NATO_Phonetic_Alphabet.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+letters = {
+    "A": "Alpha",   "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",   "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",    "H": "Hotel",   "I": "India",
+    "J": "Juliett", "K": "Kilo",    "L": "Lima",
+    "M": "Mike",    "N": "November","O": "Oscar",
+    "P": "Papa",    "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra",  "T": "Tango",   "U": "Uniform",
+    "V": "Victor",  "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee",  "Z": "Zulu"
+  }
+
+
+def nato(word):
+    return ' '.join([letters[letter] for letter in word.upper()])
+
+
+class Test(TestCase):
+    def test_1(self):
+        self.assertEqual(nato("babble"), "Bravo Alpha Bravo Bravo Lima Echo")
